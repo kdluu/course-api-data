b9a0e3e26725132ca0503d5529d2e97231b69988
Khanh Luu <khanhluu101@gmail.com>
2025-01-15 00:26:21 -0500

    Updated topic services using CrudRepository methods

--- src/main/java/com/kdluu/service/TopicService.java
+++ src/main/java/com/kdluu/service/TopicService.java
@@ -1,44 +1,39 @@
 package com.kdluu.service;
 
 import com.kdluu.model.Topic;
+import com.kdluu.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("Java", "Java Intro", "Spring Intro Description"),
-            new Topic("Javascript", "Javascript Framework", "Javascript Framework Description")
-    ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics(){
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id){
-        return topics.stream().filter(topic -> topic.getId().toLowerCase().equalsIgnoreCase(id)).findFirst().orElse(null);
+        return topicRepository.findById(id).orElse(null);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(Topic newTopic){
-        for (int i = 0; i < topics.size(); i++){
-            Topic t = topics.get(i);
-            if (t.getId().equalsIgnoreCase(newTopic.getId())){
-                topics.set(i, newTopic);
-                return;
-            }
-        }
+       topicRepository.save(newTopic);
     }
 
     public void deleteTopic(String id){
-       topics.removeIf(t -> t.getId().equalsIgnoreCase(id));
+       topicRepository.deleteById(id);
     }
 }
